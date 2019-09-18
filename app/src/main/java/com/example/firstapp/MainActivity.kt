bf88f43f11657836f76481e4b9a9d5a7845d3bcf
thembig <malilfajar17@gmail.com>
2019-09-18 09:18:07 +0700

    my first app android in codelabs

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.firstapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun toastMe(view: View) {
+        val myToast = Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe(view: View){
+        // Get the text view
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        showCountTextView.text = count.toString()
+    }
+
+    fun randomMe(view: View) {
+        val randomIntent = Intent(this, SecondActivity::class.java)
+        // Get the text view
+        val showCountTextView = findViewById<TextView>(R.id.textView)
+
+        // Get the value of the text view.
+        val countString = showCountTextView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+        startActivity(randomIntent)
+    }
+}
